56b5c128bc002a095b0ba29934bc2ee52f07fc60
Corey <corey260596@gmail.com>
2023-09-27 13:20:57 +1000

    added preview headers

--- src/Components/TablePreview/TablePreview.jsx
+++ src/Components/TablePreview/TablePreview.jsx
@@ -1,14 +1,35 @@
 import React from "react";
+import classes from "./TablePreview.module.css";
 
 const TablePreview = () => {
-  const DUMMY_TABLE = [4, 5, 6, 7, 8, 9, 10];
+  const DUMMY_TABLE = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+  const columns = 7; // Number of columns
+
+  const rows = Math.ceil(DUMMY_TABLE.length / columns); // Calculate the number of rows
+
+  // Create an array of arrays to represent rows and columns
+  const tableMatrix = Array.from({ length: rows }, (_, rowIndex) =>
+    DUMMY_TABLE.slice(rowIndex * columns, (rowIndex + 1) * columns)
+  );
+
+  const dayHeaders = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
   return (
-    <div>
+    <div className={classes.container}>
       TablePreview
-      <div>MON TUE WED THU FRI SAT SUN</div>
-      <div>
-        {DUMMY_TABLE.map((e) => (
-          <button>{e}</button>
+      <div className={classes.table}>
+        {dayHeaders.map((cell) => (
+          <button key={cell} className={classes.dayHeader}>
+            {cell}
+          </button>
+        ))}
+        {tableMatrix.map((row, rowIndex) => (
+          <div key={rowIndex} className={classes.rows}>
+            {row.map((cell) => (
+              <button key={cell} className={classes.tableButton}>
+                {cell}
+              </button>
+            ))}
+          </div>
         ))}
       </div>
     </div>
