ae08e2552d66183b959557ec1a5d0322a66916c9
Corey <corey260596@gmail.com>
2023-10-04 21:38:15 +1000

    invite copy button

--- src/Components/UserInvite/UserInvite.jsx
+++ src/Components/UserInvite/UserInvite.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UserInvite = () => {
+  const [link, setLink] = useState();
+
+  const handleCopy = () => {
+    const inviteLink = document.getElementById("link").value;
+    navigator.clipboard.writeText(inviteLink);
+    // have another way of indicating copy worked !
+    // alert("Link copied to clipboard: " + inviteLink);
+  };
+
   return (
     <div>
       UserInvite
       <p>Invite Your Friends!</p>
       <div>
-        <input readOnly={true} placeholder="Hover to reveal link" />
-        <button>Copy</button>
+        <input
+          id="link"
+          readOnly={true}
+          placeholder="Hover to reveal link"
+          value={link}
+        />
+        <button onClick={handleCopy}>Copy</button>
       </div>
     </div>
   );
